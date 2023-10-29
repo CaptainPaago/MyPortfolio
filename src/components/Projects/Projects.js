ef66ceb97d2bab70b8a429d82b8519dc54a7797a
Dele Paago <delebliss42@gmail.com>
2023-10-29 13:56:41 +0000

    Added ChanceCentral to Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -60,10 +60,10 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="ChanceCentral"
+              description="Opportunity Board crafted with Next js , MongoDB , Tailwind css , Redux toolkit. Users can find or post Jobs, Scholarships, Fellowships and Auctions on this application with timely prompts delivered to their inbox"
+              ghLink="https://github.com/captainpaago/ChanceCentral"
+              demoLink="https://chance-central.vercel.app/"
             />
           </Col>
 
