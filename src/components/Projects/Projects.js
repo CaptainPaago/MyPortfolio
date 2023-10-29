73f6d40aa52e63ac5e30281bcf6578d98fda3bae
Dele Paago <delebliss42@gmail.com>
2023-10-29 13:41:32 +0000

    Added ReadyBooks to Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -47,10 +47,12 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="ReadyBooks"
+              description="ReadyBooks, built with TypeScript in the React.js framework, takes 
+              inspiration from popular book applications such as npr.org. It is built 
+              for users who want new books to read from the most recent New York Times Best Sellers lists."
+              ghLink="https://github.com/captainpaago/readybooks"
+              demoLink="https://readybooks.vercel.app/"              
             />
           </Col>
 
